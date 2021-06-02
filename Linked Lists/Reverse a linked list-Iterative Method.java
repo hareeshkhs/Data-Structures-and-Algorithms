77800074f682650b0076a91d9e511570784ea370
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-02 08:30:54 +0530

    Add files via upload

---
+++ Linked Lists/Reverse a linked list-Iterative Method.java
@@ -0,0 +1,111 @@
+/*
+Reverse a linked list
+ 
+Given pointer to the head node of a linked list, the task is to reverse the linked list. We need to reverse the list by changing the links between nodes.
+
+Examples: 
+
+Input: Head of following linked list 
+1->2->3->4->NULL 
+Output: Linked list should be changed to, 
+4->3->2->1->NULL
+
+Input: Head of following linked list 
+1->2->3->4->5->NULL 
+Output: Linked list should be changed to, 
+5->4->3->2->1->NULL
+
+Input: NULL 
+Output: NULL
+
+Input: 1->NULL 
+Output: 1->NULL 
+*/
+
+// Java program for reversing the linked list
+//Method-1:
+//Iterative Method
+
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+   static void printList(Node node) 
+   { 		
+        while (node != null) 
+	{ 	
+                System.out.print(node.data + " "); 
+		node = node.next; 
+	} 
+	System.out.println(); 
+    }
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        
+        	int n = sc.nextInt();
+        
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+        
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        
+            Solution ob = new Solution();
+            head = ob.reverseList(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   // } Driver Code Ends
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int value;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node node)
+    {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
+}
