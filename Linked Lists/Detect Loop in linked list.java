9cb79e4edb5b059ad4d547f0ae8812abe7e9394d
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-02 13:33:15 +0530

    Update Detect Loop in linked list.java

--- Linked Lists/Detect Loop in linked list.java
+++ Linked Lists/Detect Loop in linked list.java
@@ -1,4 +1,6 @@
 /*
+Link: https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1
+
 Given a linked list of N nodes. The task is to check if the linked list has a loop. Linked list can contain self loop.
 
 Example 1:
