5daae31fcc6f50a1ea575f9736f1abe11a4d6acf
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-02 13:37:55 +0530

    Add files via upload

---
+++ Linked Lists/Remove duplicates from an unsorted linked list.java
@@ -0,0 +1,140 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1
+
+Given an unsorted linked list of N nodes. The task is to remove duplicate elements from this unsorted Linked List. When a value appears in multiple nodes, the node which appeared first should be kept, all others duplicates are to be removed.
+
+Example 1:
+
+Input:
+N = 4
+value[] = {5,2,2,4}
+Output: 5 2 4
+Explanation:Given linked list elements are
+5->2->2->4, in which 2 is repeated only.
+So, we will delete the extra repeated
+elements 2 from the linked list and the
+resultant linked list will contain 5->2->4
+
+Example 2:
+
+Input:
+N = 5
+value[] = {2,2,2,2,2}
+Output: 2
+Explanation:Given linked list elements are
+2->2->2->2->2, in which 2 is repeated. So,
+we will delete the extra repeated elements
+2 from the linked list and the resultant
+linked list will contain only 2.
+*/
+
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+class Remove_Duplicate_From_LL
+{
+    Node head;  
+    Node temp;
+	public void addToTheLast(Node node) 
+	{
+
+	  if (head == null) 
+	  {
+	    head = node;
+	    temp = node;
+	  }
+	  else{
+	      temp.next = node;
+	      temp = node;
+	  }
+	}
+
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+       
+        
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		  
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL();
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+		//llist.printList();	
+        Solution g = new Solution();
+		llist.head = g.removeDuplicates(llist.head);
+		llist.printList();
+		
+        t--;		
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+*/
+
+class Solution
+{
+    //Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) 
+    {
+        HashSet<Integer> hs = new HashSet<Integer>();
+     
+        /* Pick elements one by one */
+        Node current = head;
+        Node prev = null;
+        while (current != null)
+        {
+            if (hs.contains(current.data)) {
+                prev.next = current.next;
+            } else {
+                hs.add(current.data);
+                prev = current;
+            }
+            current = current.next;
+        }
+        return head;
+    }
+}
