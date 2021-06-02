76a0a4eda2308eed037cbab62c4b4c7adc080b75
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-02 13:24:39 +0530

    Update Reverse a linked list-Recursive Method.java

--- Linked Lists/Reverse a linked list-Recursive Method.java
+++ Linked Lists/Reverse a linked list-Recursive Method.java
@@ -1,4 +1,6 @@
 /*
+Link: https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
 Reverse a linked list
  
 Given pointer to the head node of a linked list, the task is to reverse the linked list. We need to reverse the list by changing the links between nodes.
