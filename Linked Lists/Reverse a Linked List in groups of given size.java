b0f35510ccbd75b1c680543a10f844daed44bc14
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-02 13:32:08 +0530

    Update Reverse a Linked List in groups of given size.java

--- Linked Lists/Reverse a Linked List in groups of given size.java
+++ Linked Lists/Reverse a Linked List in groups of given size.java
@@ -1,4 +1,6 @@
 /*
+Link: https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+
 Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list.
 
 Example 1:
