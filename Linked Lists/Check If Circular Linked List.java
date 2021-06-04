46663e9367eba42c119348ea66503a5e0e7c9c87
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-04 21:59:58 +0530

    Add files via upload

---
+++ Linked Lists/Check If Circular Linked List.java
@@ -0,0 +1,114 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/circular-linked-list/1
+
+Given a singly linked list, find if the linked list is circular or not. A linked list is called circular if it not NULL terminated and all nodes are connected in the form of a cycle. An empty linked list is considered as circular.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+(the first and last node is connected,
+i.e. 5 --> 1)
+Output: 1
+Example 2:
+
+Input:
+LinkedList: 2->4->6->7->5->1
+Output: 0
+
+*/
+
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinklIst_Circular
+{
+	Node head;
+	
+	
+	public static void main(String args[])
+	{
+	    Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int l = sc.nextInt();
+			
+			sc.nextLine().trim().split(" ");
+			String str[]= sc.nextLine().trim().split(" ");
+            Node head = null, tail = null;
+            head = new Node(Integer.parseInt(str[0]));
+            tail = head;
+            for(int i = 1; i < n ; i++)
+            {
+                tail.next = new Node(Integer.parseInt( str[i]));
+                tail = tail.next;
+            }
+                
+			
+			if(l==1 && n>=1)
+			{
+				Node temp = head;
+				while(temp.next != null)
+				{
+					temp = temp.next; 
+				}
+				temp.next = head;
+			}
+			
+			GfG g = new GfG();
+			boolean b = g.isCircular(head);
+			if(b==true)
+			System.out.println(1);
+			else 
+			System.out.println(0);
+		t--;
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+/* Structure of LinkedList
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+        Node start = head;
+        while(1 == 1){
+            //System.out.println(start.data);
+            if(start.next==null){
+                return false;
+            }
+            else if(start.next==head){
+                return true;
+            }
+            start = start.next;
+        }
+        //return false;
+    }
+}
