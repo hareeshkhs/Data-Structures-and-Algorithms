6ba601b0b4f24c28985153a4b5ca44597410f384
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-07 16:15:56 +0530

    Add files via upload

---
+++ Linked Lists/Given a linked list of 0s, 1s and 2s, sort it.java
@@ -0,0 +1,141 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1
+
+Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
+
+Example 1:
+
+Input:
+N = 8
+value[] = {1,2,2,1,2,0,2,2}
+Output: 0 1 1 2 2 2 2 2
+Explanation: All the 0s are segregated
+to the left end of the linked list,
+2s to the right end of the list, and
+1s in between.
+Example 2:
+
+Input:
+N = 4
+value[] = {2,2,0,1}
+Output: 0 1 2 2
+Explanation: After arranging all the
+0s,1s and 2s in the given format,
+the output will be 0 1 2 2.
+
+*/
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().segregate(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    static Node segregate(Node head)
+    {
+        //Add code here
+        Node crt=head;
+        int zero=0,one=0,two=0;
+        while(crt!=null)
+        {
+            if(crt.data==0) zero++;
+            else if(crt.data==1) one++;
+            else two++;
+            
+            crt=crt.next;
+        }
+        
+        crt=head;
+        while(crt!=null)
+        {
+            if(zero>0)
+            {
+                crt.data=0;
+                zero--;
+            }
+            else if(one>0)
+            {
+                crt.data=1;
+                one--;
+            }
+            else
+            {
+                crt.data=2;
+                two--;
+            }
+            crt=crt.next;
+        }
+        return head;
+    }
+}
