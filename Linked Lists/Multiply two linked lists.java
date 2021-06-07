412c5f81b01c75197480906ce0f7ee550cf94f8b
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-07 16:16:20 +0530

    Add files via upload

---
+++ Linked Lists/Multiply two linked lists.java
@@ -0,0 +1,135 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/multiply-two-linked-lists/1
+
+Given elements as nodes of the two linked lists. The task is to multiply these two linked lists, say L1 and L2. 
+
+Note: The output could be large take modulo 109+7.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow, the first line of each test case contains an integer N denoting the size of the first linked list (L1). In the next line are the space separated values of the first linked list. The third line of each test case contains an integer M denoting the size of the second linked list (L2). In the forth line are space separated values of the second linked list.
+
+Output:
+For each test case output will be an integer denoting the product of the two linked list.
+
+User Task:
+The task is to complete the function multiplyTwoLists() which should multiply the given two linked lists and return the result.
+
+Constraints:
+1 <= T <= 100
+1 <= N, M <= 100
+
+Example:
+Input:
+2
+2
+3 2
+1
+2
+3
+1 0 0
+2
+1 0 
+
+Output:
+64
+1000
+
+Explanation:
+Testcase 1: 32*2 = 64.
+
+Testcase 2: 100*10 = 1000.
+
+*/
+
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+class MultiplyTwoLL{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Node head=null;
+			Node phead=null;
+			int n=sc.nextInt();
+			while(n-->0){
+				int n1=sc.nextInt();
+				if(head==null)
+				head=new Node(n1);
+			else{
+				addHead(head,n1);
+			}
+			}
+			int m=sc.nextInt();
+			while(m-->0){
+				int n1=sc.nextInt();
+				if(phead==null)
+				phead=new Node(n1);
+			else{
+				addPhead(phead,n1);
+			}
+			}
+		GfG g=new GfG();
+
+System.out.println(g.multiplyTwoLists(head,phead));
+		}
+	}
+    public static void addHead(Node node,int a) {
+          if (node == null)
+            return;
+		if(node.next==null)
+			node.next=new Node(a);
+		else
+			addHead(node.next,a);
+	}
+	public static void addPhead(Node node,int a){
+		 if (node == null)
+            return;
+		if(node.next==null)
+		node.next=new Node(a);
+	else
+		addPhead(node.next,a);
+	}
+	}
+	
+	// } Driver Code Ends
+
+
+/*Node is defined as
+class Node
+{
+    int data; 
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class GfG{
+  /*You are required to complete this method */
+   public long multiplyTwoLists(Node l1,Node l2){
+          //add code here.
+          long p = 0l;
+          long q = 0l;
+          long mul = 0l;
+          while(l1!=null){
+              p = (p*10+l1.data)%1000000007;
+              l1 = l1.next;
+          }
+          while(l2!=null){
+              q = (q*10+l2.data)%1000000007;
+              l2 = l2.next;
+          }
+          mul = (p*q)%1000000007;
+          return mul;
+   }
+}
