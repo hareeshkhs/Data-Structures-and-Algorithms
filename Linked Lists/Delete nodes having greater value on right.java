80b3b9809ab2ef12d196cc5c5dc0e20ae77edc40
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-08 07:37:40 +0530

    Add files via upload

---
+++ Linked Lists/Delete nodes having greater value on right.java
@@ -0,0 +1,117 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/delete-nodes-having-greater-value-on-right/1
+
+Given a singly linked list, remove all the nodes which have a greater value on its following nodes.
+
+Example 1:
+
+Input:
+LinkedList = 12->15->10->11->5->6->2->3
+Output: 15 11 6 3
+Explanation: Since, 12, 10, 5 and 2 are
+the elements which have greater elements
+on their next node. So, after deleting
+them, the linked list would like be 15,
+11, 6, 3.
+
+Example 2:
+
+Input:
+LinkedList = 10->20->30->40->50->60
+Output: 60
+
+*/
+
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head){
+        if(head == null || head.next == null) 
+            return head;
+        Node nextNode = compute(head.next);
+ 
+        if(nextNode.data > head.data)
+            return nextNode;
+        head.next = nextNode;
+ 
+        return head;
+    }
+}
+  
