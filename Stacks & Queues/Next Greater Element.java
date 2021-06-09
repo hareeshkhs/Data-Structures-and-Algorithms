8dcc9821128a9766d58fdab22662f5adc01f1ca5
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-09 13:36:06 +0530

    Add files via upload

---
+++ Stacks & Queues/Next Greater Element.java
@@ -0,0 +1,89 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+Given an array arr[ ] of size N having distinct elements, the task is to find the next greater element for each element of the array in order of their appearance in the array.
+Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+If there does not exist next greater of current element, then next greater element for current element is -1. For example, next greater of the last element is always -1.
+
+Example 1:
+
+Input: 
+N = 4, arr[] = [1 3 2 4]
+Output:
+3 4 4 -1
+Explanation:
+In the array, the next larger element 
+to 1 is 3 , 3 is 4 , 2 is 4 and for 4 ? 
+since it doesn't exist, it is -1.
+
+Example 2:
+
+Input: 
+N = 5, arr[] [6 8 0 1 3]
+Output:
+8 -1 1 3 -1
+Explanation:
+In the array, the next larger element to 
+6 is 8, for 8 there is no larger elements 
+hence it is -1, for 0 it is 1 , for 1 it 
+is 3 and then for 3 there is no larger 
+element on right and hence -1.
+
+*/
+
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    long[] res = new Solution().nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        Stack<Long> s = new Stack<>();
+        long nge[] = new long[n];
+ 
+        for (int i = arr.length - 1; i >= 0; i--)
+        {
+            if (!s.empty())
+            {
+                while (!s.empty() && s.peek() <= arr[i])
+                {
+                    s.pop();
+                }
+            }
+            nge[i] = s.empty() ? -1 : s.peek();
+            s.push(arr[i]);
+        }
+        /*
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(nge[i]+ " ");
+        */
+        return nge;
+    } 
+}
