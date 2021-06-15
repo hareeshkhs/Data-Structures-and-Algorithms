2967eccc81a0716e1b0af013d493fcc289203078
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-15 17:47:48 +0530

    Add files via upload

---
+++ Arrays/Count pairs with given sum.java
@@ -0,0 +1,79 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1
+
+Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+
+
+Example 1:
+
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+Explanation: 
+arr[0] + arr[1] = 1 + 5 = 6 
+and arr[1] + arr[3] = 5 + 1 = 6.
+
+Example 2:
+
+Input:
+N = 4, X = 2
+arr[] = {1, 1, 1, 1}
+Output: 6
+Explanation: 
+Each 1 will produce sum 2 with any 1.
+
+*/
+
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int ans = new Solution().getPairsCount(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int getPairsCount(int[] arr, int n, int sum) {
+        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+        for (int i = 0; i < n; i++) {
+
+			// initializing value to 0, if key not found
+			if (!hm.containsKey(arr[i]))
+				hm.put(arr[i], 0);
+
+			hm.put(arr[i], hm.get(arr[i]) + 1);
+		}
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			if (hm.get(sum - arr[i]) != null)
+				count += hm.get(sum - arr[i]);
+           
+			if (sum - arr[i] == arr[i])
+				count--;
+		}
+		return count / 2;
+    }
+}
