eb70e038df2d31de1f475d806b337bffbc849757
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-15 17:44:42 +0530

    Add files via upload

---
+++ Arrays/Next Permutation.java
@@ -0,0 +1,53 @@
+/*
+Link: https://leetcode.com/problems/next-permutation/
+
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+
+*/
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if(nums == null || nums.length <= 1) return;
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]) i--;
+        if(i >= 0) {
+            int j = nums.length - 1;              // Start from the end
+            while(nums[j] <= nums[i]) j--;           // Find rightmost first larger id j
+            swap(nums, i, j);                     // Switch i and j
+        }
+        reverse(nums, i + 1, nums.length - 1);       // Reverse the descending sequence
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public void reverse(int[] nums, int i, int j) {
+        while(i < j) swap(nums, i++, j--);
+    }
+}
