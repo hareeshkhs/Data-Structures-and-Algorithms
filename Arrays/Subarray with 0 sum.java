fcd2692308a1d228af02d8d2cd723d83173ef5f7
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-15 17:56:14 +0530

    Add files via upload

---
+++ Arrays/Subarray with 0 sum.java
@@ -0,0 +1,96 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/subarray-with-0-sum-1587115621/1
+
+Given an array of positive and negative numbers. Find if there is a subarray (of size at-least one) with 0 sum.
+
+Example 1:
+
+Input:
+5
+4 2 -3 1 6
+
+Output: 
+Yes
+
+Explanation: 
+2, -3, 1 is the subarray 
+with sum 0.
+Example 2:
+
+Input:
+5
+4 2 0 1 6
+
+Output: 
+Yes
+
+Explanation: 
+0 is one of the element 
+in the array so there exist a 
+subarray with sum 0.
+
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+			//code
+			
+		//taking input using class Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		//taking total number of testcases
+		int t = scan.nextInt();
+		while(t>0)
+		{
+		    //taking total count of elements
+		    int n = scan.nextInt() ;
+		    
+		    //Declaring and Initializing an array of size n
+		    int arr[] = new int[n];
+		    
+		    //adding elements to the array
+		    for(int i = 0; i<n;i++)
+		    {
+		        arr[i] = scan.nextInt();
+		    }
+		    
+		    t--;
+		    
+		     //calling the method findSum
+		     //and print "YES" or "NO" based on the result
+		     System.out.println(new Solution().findsum(arr,n)==true?"Yes":"No");
+		}
+	}
+	
+	
+}// } Driver Code Ends
+
+
+
+class Solution{
+    
+    static boolean findsum(int arr[],int n)
+    {
+        HashSet<Integer> hs = new HashSet<Integer>();
+ 
+        int sum = 0;
+ 
+        for (int i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+ 
+            if (arr[i] == 0
+                || sum == 0
+                || hs.contains(sum))
+                return true;
+ 
+            hs.add(sum);
+        }
+        return false;
+    }
+}
