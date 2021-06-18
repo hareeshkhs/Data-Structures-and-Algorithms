e91be49ef3abda9c224377dea7c42b3c8db4c8f6
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-18 08:51:21 +0530

    Add files via upload

---
+++ Strings/Count the Reversals.java
@@ -0,0 +1,99 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/count-the-reversals0401/1
+
+Given a string S consisting of only opening and closing curly brackets '{' and '}', find out the minimum number of reversals required to convert the string into a balanced expression.
+A reversal means changing '{' to '}' or vice-versa.
+
+Example 1:
+
+Input:
+S = "}{{}}{{{"
+Output: 3
+Explanation: One way to balance is:
+"{{{}}{}}". There is no balanced sequence
+that can be formed in lesser reversals.
+â€‹Example 2:
+
+Input: 
+S = "{{}{{{}{{}}{{"
+Output: -1
+Explanation: There's no way we can balance
+this sequence of braces.
+
+*/
+
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+    		System.out.println (new Sol().countRev (s));
+        }
+        
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    int countRev (String expr)
+    {
+        int len = expr.length();
+     
+        // length of expression must be even to make
+        // it balanced by using reversals.
+        if (len%2 != 0)
+        return -1;
+     
+        // After this loop, stack contains unbalanced
+        // part of expression, i.e., expression of the
+        // form "}}..}{{..{"
+        Stack<Character> s=new Stack<>();
+         
+        for (int i=0; i<len; i++)
+        {
+            char c = expr.charAt(i);
+            if (c =='}' && !s.empty())
+            {
+                if (s.peek()=='{')
+                    s.pop();
+                else
+                    s.push(c);
+            }
+            else
+                s.push(c);
+        }
+     
+        // Length of the reduced expression
+        // red_len = (m+n)
+        int red_len = s.size();
+     
+        // count opening brackets at the end of
+        // stack
+        int n = 0;
+        while (!s.empty() && s.peek() == '{')
+        {
+            s.pop();
+            n++;
+        }
+     
+        // return ceil(m/2) + ceil(n/2) which is
+        // actually equal to (m+n)/2 + n%2 when
+        // m+n is even.
+        return (red_len/2 + n%2);    
+    }
+}
