1134f30a3dd9f51cfcf02c6d61d039805b1a62ac
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-18 08:55:51 +0530

    Add files via upload

---
+++ Strings/Reverse String.java
@@ -0,0 +1,31 @@
+/*
+Link: https://leetcode.com/problems/reverse-string/
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+*/
+
+class Solution {
+    public void reverseString(char[] word) {
+        int i = 0;
+        int j = word.length - 1;
+        while (i < j) {
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
