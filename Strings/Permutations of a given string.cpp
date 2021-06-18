af7a432d3a66f26aaabb121353f669683d7184f1
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-18 08:55:09 +0530

    Add files via upload

---
+++ Strings/Permutations of a given string.cpp
@@ -0,0 +1,69 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/permutations-of-a-given-string2041/1
+
+Given a string S. The task is to print all permutations of a given string.
+
+
+Example 1:
+
+Input: ABC
+Output:
+ABC ACB BAC BCA CAB CBA
+Explanation:
+Given string ABC has permutations in 6 
+forms as ABC, ACB, BAC, BCA, CAB and CBA .
+Example 2:
+
+Input: ABSG
+Output:
+ABGS ABSG AGBS AGSB ASBG ASGB BAGS 
+BASG BGAS BGSA BSAG BSGA GABS GASB 
+GBAS GBSA GSAB GSBA SABG SAGB SBAG 
+SBGA SGAB SGBA
+Explanation:
+Given string ABSG has 24 permutations.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+        vector<string>find_permutation(string S)
+        {
+            vector<string>res;
+            // sort the string in lexicographically order
+            sort(S.begin(), S.end());
+            do
+            {
+                // keep adding while there is next permutation
+                res.push_back(S);
+            }while(next_permutation(S.begin(), S.end()));
+            return res;
+        }
+};
+
+
+
+// { Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends
