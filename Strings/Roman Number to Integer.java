a593806277104606599f7c6db9c16fb757a5636c
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-19 15:25:42 +0530

    Add files via upload

---
+++ Strings/Roman Number to Integer.java
@@ -0,0 +1,103 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/roman-number-to-integer3201/1
+
+Given a string in roman no format (s)  your task is to convert it to an integer . Various symbols and their values are given below.
+I 1
+V 5
+X 10
+L 50
+C 100
+D 500
+M 1000
+
+Example 1:
+
+Input:
+s = V
+Output: 5
+Example 2:
+
+Input:
+s = III 
+Output: 3
+
+*/
+
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String roman = br.readLine().trim();
+            RomanToNumber rn = new RomanToNumber();
+            System.out.println(rn.romanToDecimal(roman));
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class RomanToNumber {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+        int res = 0;
+ 
+        for (int i = 0; i < str.length(); i++)
+        {
+            // Getting value of symbol s[i]
+            int s1 = value(str.charAt(i));
+ 
+            // Getting value of symbol s[i+1]
+            if (i + 1 < str.length())
+            {
+                int s2 = value(str.charAt(i + 1));
+ 
+                // Comparing both values
+                if (s1 >= s2)
+                {
+                    // Value of current symbol
+                    // is greater or equalto
+                    // the next symbol
+                    res = res + s1;
+                }
+                else
+                {
+                    // Value of current symbol is
+                    // less than the next symbol
+                    res = res + s2 - s1;
+                    i++;
+                }
+            }
+            else {
+                res = res + s1;
+            }
+        }
+        return res;
+    }
+    static int value(char r)
+    {
+        if (r == 'I')
+            return 1;
+        if (r == 'V')
+            return 5;
+        if (r == 'X')
+            return 10;
+        if (r == 'L')
+            return 50;
+        if (r == 'C')
+            return 100;
+        if (r == 'D')
+            return 500;
+        if (r == 'M')
+            return 1000;
+        return -1;
+    }
+}
