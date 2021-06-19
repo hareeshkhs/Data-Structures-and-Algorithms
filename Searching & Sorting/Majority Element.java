de13abd804578daebfdb9d55a4da86d7830fc56d
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-19 16:34:00 +0530

    Add files via upload

---
+++ Searching & Sorting/Majority Element.java
@@ -0,0 +1,84 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/majority-element-1587115620/1
+
+Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+ 
+
+Example 1:
+
+Input:
+N = 3 
+A[] = {1,2,3} 
+Output:
+-1
+Explanation:
+Since, each element in 
+{1,2,3} appears only once so there 
+is no majority element.
+Example 2:
+
+Input:
+N = 5 
+A[] = {3,1,3,3,2} 
+Output:
+3
+Explanation:
+Since, 3 is present more
+than N/2 times, so it is 
+the majority element.
+
+*/
+
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int arr[], int size)
+    {
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+ 
+        for(int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                    int count = map.get(arr[i]) +1;
+                    if (count > arr.length /2) {
+                        return arr[i];
+                    } else
+                        map.put(arr[i], count);
+ 
+            }
+            else
+                map.put(arr[i],1);
+            }
+            if(arr.length==1){
+                return arr[0];
+            }
+            return -1;
+    }
+}
