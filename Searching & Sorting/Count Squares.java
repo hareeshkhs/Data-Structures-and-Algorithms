376af609110be945af0b6f5d8f5348836b94e914
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-19 16:31:21 +0530

    Add files via upload

---
+++ Searching & Sorting/Count Squares.java
@@ -0,0 +1,56 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/count-squares3649/1
+
+Consider a sample space S consisting of all perfect squares starting from 1, 4, 9 and so on. You are given a number N, you have to output the number of integers less than N in the sample space S.
+
+ 
+
+Example 1:
+
+Input :
+N = 9
+Output:
+2
+Explanation:
+1 and 4 are the only Perfect Squares
+less than 9. So, the Output is 2.
+Example 2:
+
+Input :
+N = 3
+Output:
+1
+Explanation:
+1 is the only Perfect Square
+less than 3. So, the Output is 1.
+
+*/
+
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.countSquares(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int countSquares(int N) {
+        int turn = (int)Math.sqrt(N-1);
+        return turn;
+    }
+};
