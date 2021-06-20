2d781dedee9d60e3df05c2d969c0894d1f290572
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-20 09:37:23 +0530

    Add files via upload

---
+++ Searching & Sorting/Value equal to index value.java
@@ -0,0 +1,70 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/value-equal-to-index-value1330/1
+
+Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value.
+
+Example 1:
+
+Input: 
+N = 5
+Arr[] = {15, 2, 45, 12, 7}
+Output: 2
+Explanation: Only Arr[2] = 2 exists here.
+Example 2:
+
+Input: 
+N = 1
+Arr[] = {1}
+Output: 1
+Explanation: Here Arr[1] = 1 exists.
+*/
+
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            ArrayList<Integer> ans = new Solution().valueEqualToIndex(arr, n);
+            if (ans.size() == 0) {
+                System.out.println("Not Found");
+            } else {
+                for (Integer x : ans) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            if(arr[i]==i+1){
+                list.add(i+1);
+            }
+        }
+        return list;
+    }
+}
