1e38d8210354394203780d81a45d4d33c57cbf5b
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-23 06:58:56 +0530

    Add files via upload

---
+++ Binary Search Trees/Insert a node in a BST.java
@@ -0,0 +1,163 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/insert-a-node-in-a-bst/1
+
+Given a BST and a key K. If K is not present in the BST, Insert a new Node with a value equal to K into the BST. 
+Note: If K is already present in the BST, don't modify the BST.
+
+
+Example 1:
+
+Input:
+     2
+   /   \
+  1     3
+K = 4
+Output: 1 2 3 4
+Explanation: After inserting the node 4
+Inorder traversal will be 1 2 3 4.
+Example 2:
+
+Input:
+        2
+      /   \
+     1     3
+             \
+              6
+K = 4
+Output: 1 2 3 4 6
+Explanation: After inserting the node 4
+Inorder traversal of the above tree
+will be 1 2 3 4 6.
+
+*/
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            int key = Integer.parseInt(br.readLine().trim());
+            Solution T = new Solution();
+            root = T.insert(root,key);
+            inorder(root);
+            System.out.println();
+            t--;
+        }
+    }
+    
+    static void inorder(Node root)
+    {
+        if(root==null)  return;
+    
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to insert a node in a BST.
+    Node insert(Node root, int key)
+    {
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+ 
+        /* Otherwise, recur down the tree */
+        if (key < root.data)
+            root.left = insert(root.left, key);
+        else if (key > root.data)
+            root.right = insert(root.right, key);
+ 
+        /* return the (unchanged) node pointer */
+        return root;
+    }
+}
