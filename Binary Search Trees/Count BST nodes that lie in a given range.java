3116b07f41e1b1d8563d4bda576ba0d3debe37d3
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-23 06:58:11 +0530

    Add files via upload

---
+++ Binary Search Trees/Count BST nodes that lie in a given range.java
@@ -0,0 +1,167 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/count-bst-nodes-that-lie-in-a-given-range/1
+
+Given a Binary Search Tree (BST) and a range l-h(inclusive), count the number of nodes in the BST that lie in the given range.
+
+The values smaller than root go to the left side
+The values greater and equal to the root go to the right side
+Example 1:
+
+Input:
+      10
+     /  \
+    5    50
+   /    /  \
+  1    40  100
+l = 5, h = 45
+Output: 3
+Explanation: 5 10 40 are the node in the
+range
+Example 2:
+
+Input:
+     5
+    /  \
+   4    6
+  /      \
+ 3        7
+l = 2, h = 8
+Output: 5
+Explanation: All the nodes are in the
+given range.
+
+*/
+
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+	            String[] ab = br.readLine().trim().split(" ");
+                int a = Integer.parseInt(ab[0]);
+                int b = Integer.parseInt(ab[1]);
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+			    System.out.println(g.getCount(root,a,b));
+                t--;
+            
+        }
+    }
+  
+}
+
+// } Driver Code Ends
+
+
+// A Binary Search Tree node
+
+
+class Tree
+{
+    //Function to count number of nodes in BST that lie in the given range.
+    int count=0;
+    void Rangecount(Node root, int l, int h){
+        if(root == null) 
+            return;
+        if(root.data>=l)
+            Rangecount(root.left,l,h);
+        if(root.data>=l && root.data <=h)
+            count++;
+        if(root.data<=h)
+            Rangecount(root.right,l,h);
+    }
+    int getCount(Node root,int l, int h)
+    {
+        Rangecount(root,l,h);
+        return count;
+    }
+}
