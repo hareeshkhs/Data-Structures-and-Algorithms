89346a161e5f85326af2fda3a81b27c0944061d5
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-25 08:37:24 +0530

    Add files via upload

---
+++ Heap/K largest elements.java
@@ -0,0 +1,77 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/k-largest-elements3736/1
+
+Given an array of N positive integers, print k largest elements from the array. 
+
+Example 1:
+
+Input:
+N = 5, k = 2
+arr[] = {12,5,787,1,23}
+Output: 787 23
+Explanation: First largest element in
+the array is 787 and the second largest
+is 23.
+Example 2:
+
+Input:
+N = 7, k = 3
+arr[] = {1,23,12,9,30,2,50}
+Output: 50 30 23
+Explanation: Three Largest element in
+the array are 50, 30 and 23.
+
+*/
+
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0)
+        {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            
+            int arr[] = new int[n];
+            for(int i = 0; i<n; ++i)
+                arr[i] = sc.nextInt();
+            Solution ob = new Solution();
+            ArrayList<Integer> list = ob.kLargest(arr, n, k);
+            for(int i = 0; i<list.size(); i++)
+                System.out.print(list.get(i) + " ");
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to return k largest elements from an array.
+    public static ArrayList<Integer> kLargest(int arr[], int n, int k)
+    {
+        Integer[] obj_array = Arrays.stream( arr ).boxed().toArray( Integer[] :: new);
+        Arrays.sort(obj_array, Collections.reverseOrder());
+        ArrayList<Integer> list = new ArrayList<>(k);
+ 
+        for (int i = 0; i < k; i++)
+            list.add(obj_array[i]);
+     
+        return list;
+    }
+}
