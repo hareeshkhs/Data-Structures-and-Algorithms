ee6ea06cae7e759e31712d69d3d08c094818273d
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-06-25 08:39:25 +0530

    Add files via upload

---
+++ Heap/Minimum Cost of ropes.java
@@ -0,0 +1,139 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+
+There are given N ropes of different lengths, we need to connect these ropes into one rope. The cost to connect two ropes is equal to sum of their lengths. The task is to connect the ropes with minimum cost.
+
+Example 1:
+
+Input:
+n = 4
+arr[] = {4, 3, 2, 6}
+Output: 
+29
+Explanation:
+For example if we are given 4
+ropes of lengths 4, 3, 2 and 6. We can
+connect the ropes in following ways.
+1) First connect ropes of lengths 2 and 3.
+Now we have three ropes of lengths 4, 6
+and 5.
+2) Now connect ropes of lengths 4 and 5.
+Now we have two ropes of lengths 6 and 9.
+3) Finally connect the two ropes and all
+ropes have connected.
+Total cost for connecting all ropes is 5
++ 9 + 15 = 29. This is the optimized cost
+for connecting ropes. Other ways of
+connecting ropes would always have same
+or more cost. For example, if we connect
+4 and 6 first (we get three strings of 3,
+2 and 10), then connect 10 and 3 (we get
+two strings of 13 and 2). Finally we
+connect 13 and 2. Total cost in this way
+is 10 + 13 + 15 = 38.
+Example 2:
+
+Input:
+n = 5
+arr[] = {4, 2, 7, 6, 9}
+Output: 
+62 
+Explanation:
+First, connect ropes 4 and 2, which makes
+the array {6,7,6,9}. Next, add ropes 6 and
+6, which results in {12,7,9}. Then, add 7
+and 9, which makes the array {12,16}. And
+finally add these two which gives {28}.
+Hence, the total cost is 6 + 12 + 16 + 
+28 = 62.
+
+*/
+
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+        
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			long arr[] = new long[n];
+			
+			for(int i=0; i<n; i++)
+				arr[i] = sc.nextLong();
+		    out.println(new Solution().minCost(arr, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to return the minimum cost of connecting the ropes.
+    long minCost(long arr[], int n) 
+    {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+         
+        for (int i = 0; i < n; i++) pq.add(arr[i]);
+        Long cost = new Long("0");
+        
+        //using a loop while there is more than one element in priority queue. 
+        while (pq.size() != 1) 
+        {
+            //storing the first and second numbers from priority queue.
+            long x = pq.poll();  
+            long y = pq.poll(); 
+            
+            //adding their sum in result.
+            cost += (x + y);
+            
+            //pushing the sum of first and second numbers in priority queue.
+            pq.add(x + y); 
+        }
+        //returning the result.
+        return cost;
+    }
+}
