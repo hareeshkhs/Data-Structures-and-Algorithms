875b4a4137b127b53873d579fddbc0400a6a1572
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-04 12:27:38 +0530

    Add files via upload

---
+++ DS CODES/2searching in Dlinked list.c
@@ -0,0 +1,102 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+void create(int);  
+void search();  
+struct node  
+{  
+    int data;  
+    struct node *next;  
+    struct node *prev;  
+};  
+struct node *head;  
+void main ()  
+{  
+    int choice,item,loc;  
+    do   
+    {  
+        printf("\n1.Create\n2.Search\n3.Exit\n4.Enter your choice?");  
+        scanf("%d",&choice);  
+        switch(choice)  
+        {  
+            case 1:  
+            printf("\nEnter the item\n");  
+            scanf("%d",&item);  
+            create(item);  
+            break;   
+            case 2:  
+            search();  
+            case 3:  
+            exit(0);  
+            break;    
+            default:  
+            printf("\nPlease enter valid choice\n");  
+        }  
+                  
+    }while(choice != 3);  
+}  
+void create(int item)  
+{  
+   
+   struct node *ptr = (struct node *)malloc(sizeof(struct node));  
+   if(ptr == NULL)  
+   {  
+       printf("\nOVERFLOW");  
+   }  
+   else  
+   {  
+      
+      
+   if(head==NULL)  
+   {  
+       ptr->next = NULL;  
+       ptr->prev=NULL;  
+       ptr->data=item;  
+       head=ptr;  
+   }  
+   else   
+   {  
+       ptr->data=item;printf("\nPress 0 to insert more ?\n");  
+       ptr->prev=NULL;  
+       ptr->next = head;  
+       head->prev=ptr;  
+       head=ptr;  
+   }  
+    printf("\nNode Inserted\n");  
+}  
+     
+}  
+void search()  
+{  
+    struct node *ptr;  
+    int item,i=0,flag;  
+    ptr = head;   
+    if(ptr == NULL)  
+    {  
+        printf("\nEmpty List\n");  
+    }  
+    else  
+    {   
+        printf("\nEnter item which you want to search?\n");   
+        scanf("%d",&item);  
+        while (ptr!=NULL)  
+        {  
+            if(ptr->data == item)  
+            {  
+                printf("\nitem found at location %d ",i+1);  
+                flag=0;  
+                break;  
+            }   
+            else  
+            {  
+                flag=1;  
+            }  
+            i++;  
+            ptr = ptr -> next;  
+        }  
+        if(flag==1)  
+        {  
+            printf("\nItem not found\n");  
+        }  
+    }     
+          
+}  
