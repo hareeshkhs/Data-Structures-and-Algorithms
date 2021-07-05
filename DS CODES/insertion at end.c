c4b9bdb2d5a95162c6ecab0eaf2ac3ff405cfb74
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-05 08:20:31 +0530

    Add files via upload

---
+++ DS CODES/insertion at end.c
@@ -0,0 +1,52 @@
+#include<stdio.h>  
+#include<stdlib.h>  
+void lastinsert(int);  
+struct node  
+{  
+    int data;  
+    struct node *next;  
+};  
+struct node *head;  
+void main ()  
+{  
+    int choice,item;  
+    do   
+    {  
+        printf("\nEnter the item which you want to insert?\n");  
+        scanf("%d",&item);  
+        lastinsert(item);  
+        printf("\nPress 0 to insert more ?\n");  
+        scanf("%d",&choice);  
+    }while(choice == 0);  
+}  
+void lastinsert(int item)  
+    {  
+        struct node *ptr = (struct node*)malloc(sizeof(struct node));     
+        struct node *temp;  
+        if(ptr == NULL)  
+        {  
+            printf("\nOVERFLOW");     
+        }  
+        else  
+        {  
+            ptr->data = item;  
+            if(head == NULL)  
+            {  
+                ptr -> next = NULL;  
+                head = ptr;  
+                printf("\nNode inserted");  
+            }  
+            else  
+            {  
+                temp = head;  
+                while (temp -> next != NULL)  
+                {  
+                    temp = temp -> next;  
+                }  
+                temp->next = ptr;  
+                ptr->next = NULL;  
+                printf("\nNode inserted");  
+              
+            }  
+        }  
+    }  
