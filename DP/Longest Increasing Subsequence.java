a825a21d91e7ad93cfe63696d1f32de6fd254823
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-17 07:45:43 +0530

    Add files via upload

---
+++ DP/Longest Increasing Subsequence.java
@@ -0,0 +1,146 @@
+/*
+Link: https://practice.geeksforgeeks.org/problems/longest-increasing-subsequence-1587115620/1
+
+Given an array of integers, find the length of the longest (strictly) increasing subsequence from the given array.
+
+Example 1:
+
+Input:
+N = 16
+A[]={0,8,4,12,2,10,6,14,1,9,5
+     13,3,11,7,15}
+Output: 6
+Explanation:Longest increasing subsequence
+0 2 6 9 13 15, which has length 6
+Example 2:
+
+Input:
+N = 6
+A[] = {5,8,3,7,9,1}
+Output: 3
+Explanation:Longest increasing subsequence
+5 7 9, with length 3
+
+*/
+
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total testcases
+        int t = sc.nextInt();
+        while(t > 0){
+            
+            //taking size of array
+            int n = sc.nextInt();
+            int array[] = new int[n];
+            
+            //inserting elements in the array
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+            
+            //creating an object of class Solution
+            Solution ob = new Solution();
+            
+            //calling longestSubsequence() method of class
+            //Solution
+            System.out.println(ob.longestSubsequence(n,array));
+            t--;
+        }
+    } 
+} // } Driver Code Ends
+
+
+
+
+
+class Solution 
+{
+    //Function to find length of longest increasing subsequence.
+    static int CeilIndex(int A[], int l, int r, int key)
+    {
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (A[m] >= key)
+                r = m;
+            else
+                l = m;
+        }
+ 
+        return r;
+    }
+    static int longestSubsequence(int size, int A[])
+    {
+        int[] tailTable = new int[size];
+        int len; // always points empty slot
+ 
+        tailTable[0] = A[0];
+        len = 1;
+        for (int i = 1; i < size; i++) {
+            if (A[i] < tailTable[0])
+                // new smallest value
+                tailTable[0] = A[i];
+ 
+            else if (A[i] > tailTable[len - 1])
+                // A[i] wants to extend largest subsequence
+                tailTable[len++] = A[i];
+ 
+            else
+                // A[i] wants to be current end candidate of an existing
+                // subsequence. It will replace ceil value in tailTable
+                tailTable[CeilIndex(tailTable, -1, len - 1, A[i])] = A[i];
+        }
+ 
+        return len;
+    }
+}
+/*
+Dynamic Programming Java implementation of LIS problem
+ 
+class LIS {
+    /* lis() returns the length of the longest
+       increasing subsequence in arr[] of size n */
+    static int lis(int arr[], int n)
+    {
+        int lis[] = new int[n];
+        int i, j, max = 0;
+ 
+        /* Initialize LIS values for all indexes */
+        for (i = 0; i < n; i++)
+            lis[i] = 1;
+ 
+        /* Compute optimized LIS values in
+           bottom up manner */
+        for (i = 1; i < n; i++)
+            for (j = 0; j < i; j++)
+                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+ 
+        /* Pick maximum of all LIS values */
+        for (i = 0; i < n; i++)
+            if (max < lis[i])
+                max = lis[i];
+ 
+        return max;
+    }
+ 
+    public static void main(String args[])
+    {
+        int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+        int n = arr.length;
+        System.out.println("Length of lis is " + lis(arr, n)
+                           + "\n");
+    }
+}
+Complexity Analysis: 
+
+Time Complexity: O(n2). 
+As nested loop is used.
+Auxiliary Space: O(n). 
+Use of any array to store LIS values at each index.
+*/
